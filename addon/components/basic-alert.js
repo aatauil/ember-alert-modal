eeff6094e1947f7225c4632577a9b1f48d1047b6
achraf <achrafatauil@hotmail.com>
2020-08-22 12:35:35 +0200

    modified names

--- addon/components/basic-alert.js
+++ addon/components/basic-alert.js
@@ -2,14 +2,14 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
+
+
 export default class BasicAlertComponent extends Component {
 
-    @tracked modalState = true // Default closed
+    @tracked openState = true // Default closed
 
-
-    @action
-    toggleModalState(){
-        this.modalState = !this.modalState
+    @action baToggle(){
+        this.openState = !this.openState
     }
 
 }
